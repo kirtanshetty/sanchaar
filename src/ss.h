3e54f802a5425c42ad3f98b2dcdb226eec73bd55
Kirtan Shetty <kirtankshetty@gmail.com>
2017-07-02 21:42:23 +0530

    Created server socket.

--- src/ss.h
+++ src/ss.h
@@ -1,41 +1,57 @@
 #ifndef P_SS_H
 #define P_SS_H
 
-// test function
-int socket_server(void);
+#include <stdint.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 // socket server configuration for user
-struct p_server_config {
+typedef struct s_server_config {
   uint32_t buffer_size;
   uint16_t port;
-  int fd;
-};
+  int i_family;
+  int s_type;
+  int protocol;
+  uint32_t max_cli;
+} s_server_config;
 
-struct p_channels {
+typedef struct s_channels {
 
-}
+} s_channels;
 
-struct p_client {
+typedef struct s_client {
   int fd;
   struct sockaddr_in client_addr;
-};
+} s_client;
 
 // socket structue of the PUB/SUB server
-struct p_socket {
-  int welcome_socket, new_socket;
-  char buffer[1024];
+typedef struct s_socket {
+  int lfd;
+  int cfd;
+  // char buffer[1024];
   struct sockaddr_in server_addr;
-  struct sockaddr_storage server_storage;
-  socklen_t addr_size;
-};
+  // struct sockaddr_storage server_storage;
+  // socklen_t addr_size;
+  s_server_config ssc;
+} s_socket;
 
-int p_start_server(struct p_server_config psc);
-int p_stop_server(struct p_server_config psc);
+s_socket* server;
+char* buffer;
 
-int p_block_channel(const char* channel_name);
-int p_unblock_channel(const char* channel_name);
+// test function
+// int s_socket_server(void);
 
-int p_block_ip(uint32_t ip);
-int p_unblock_ip(uint32_t ip);
+int s_create_server(s_server_config*);
+void s_start_server();
+
+int s_stop_server(s_server_config*);
+
+int s_block_channel(const char* channel_name);
+int s_unblock_channel(const char* channel_name);
+
+int s_block_ip(uint32_t ip);
+int s_unblock_ip(uint32_t ip);
 
 #endif
